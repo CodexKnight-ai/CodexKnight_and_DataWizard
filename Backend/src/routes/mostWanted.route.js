8d81cf2c40a61146b82d3571141c1af43fde0218
CodexKnight-ai <148789243+CodexKnight-ai@users.noreply.github.com>
2024-08-04 11:13:26 +0530

    Minute changes

--- Backend/src/routes/mostWanted.route.js
+++ Backend/src/routes/mostWanted.route.js
@@ -7,6 +7,7 @@ import {
   deleteCriminal,
   updateCriminalAvatar,
 } from '../controllers/mostWanted.controller.js';
+import { verifyJWT } from '../middlewares/auth.middleware.js';
 
 const router = express.Router();
 
